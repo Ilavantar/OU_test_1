8c6919b9e5ca68feb85e12dc780c467ee385064a
Maciek <ilavantar55@gmail.com>
2022-10-07 15:31:49 +0200

    odejmowanie

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 
 
 int Policz(int a, int b){
-    return a * b; // Zmiana dodawania na mnożenie
+    return a - b; // Zmiana dodawania na odejmowanie
 }
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "W drugim branchu; 2 * 3 = " << Policz(2, 3);
+    std::cout << "W trzecim branchu; 2 - 3 = " << Policz(2, 3);
     return 0;
 }
